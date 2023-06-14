a1e95a652cce1251a26694764028bb9d8939c1c6
cjamar <121177196+cjamar@users.noreply.github.com>
2023-06-14 13:22:54 +0200

    Add Header to App

--- galeria/src/App.js
+++ galeria/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/static/home/Home';
 import Auth from './pages/auth/Auth';
+import Header from './layout/header/Header';
 
 
 function App() {
   return (
     <Router>
+      <Header></Header>
       <Routes>
       <Route path='' element={<Home />} ></Route>
       <Route path='/auth' element={<Auth />}></Route>
