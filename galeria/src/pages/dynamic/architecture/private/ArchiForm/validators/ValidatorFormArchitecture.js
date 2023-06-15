f9b187b2dd6773599486c84af10421cbdf9586b5
JuanI <qis64129@educastur.es>
2023-06-15 19:33:45 +0200

    small changes to the footer to avoid eslint errors, created validators for the architecture form, and a separate validator class.

--- galeria/src/pages/dynamic/architecture/private/ArchiForm/validators/ValidatorFormArchitecture.js
+++ galeria/src/pages/dynamic/architecture/private/ArchiForm/validators/ValidatorFormArchitecture.js
@@ -10,18 +10,16 @@ class ValidatorArchitecture{
   
     
 
-    //el msg es el mensaje que queremos mostrar si no cumple
-    //metodos para validaciones
     isNotEmpty(msg)
+    //recibe el posible mensaje de error, comprueba si el valor es falsy.
     {
         if(!this.value)
         {
             this.result.push(msg);
         }
-        //Devolvemos el objeto validador
         return this;
     }
-    //Longitudes
+    //recibe longitud minima, maxima y el mensaje de error, comprueba lo apropiado respectivamente.
     isLength(minLen,maxLen, msg)
     {
         if(this.value.length < minLen || this.value.length > maxLen)
@@ -31,14 +29,30 @@ class ValidatorArchitecture{
         return this;
     }
     
-    //requerido
+//comprueba si el campo queda vacío, recibe el mensaje de error.
     isRequired(msg)
     {
-        if(this.value.length == 0){
+        if(this.value.length === 0){
             this.result.push(msg);
         }
         return this;
     }
+//Comprueba a ver si al convertir el valor pasado al validador a numero sale NaN, en cuyo caso da error.
+    isNumber(msg){
+        if(isNaN(+this.value)){
+            this.result.push(msg);
+        }
+        return this;
+    }
+//comprueba si el año introducido es futuro
+    isNotFuture(msg){
+        if(this.value > new Date().getFullYear() ){
+            this.result.push(msg);
+        }
+        return this;
+    }
+
+
 }
 
 export default ValidatorArchitecture;
