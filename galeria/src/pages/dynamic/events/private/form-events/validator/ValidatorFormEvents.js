5262952f769c07d3b9d1699ce948d42ef83d7265
cjamar <121177196+cjamar@users.noreply.github.com>
2023-06-15 15:00:30 +0200

    Create ValidatorFormEvents

---
+++ galeria/src/pages/dynamic/events/private/form-events/validator/ValidatorFormEvents.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+
+class ValidatorFormEvents extends Component{
+
+    constructor(value){
+        super();
+        this.value = value;
+    }
+}
+
+export default ValidatorFormEvents;
