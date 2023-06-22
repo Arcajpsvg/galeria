654dbac978ac0b2a530c81539195c5cc2564fc5f
cjamar <121177196+cjamar@users.noreply.github.com>
2023-06-22 15:04:51 +0200

    Add eventsForm path & Add link button from EventList to EventForm

--- galeria/src/App.js
+++ galeria/src/App.js
@@ -1,31 +1,31 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './pages/static/home/Home';
-import Auth from './pages/auth/Auth';
-import Header from './layout/header/Header';
-import NotFound from './pages/static/notFound/NotFound';
-import ArchiList from './pages/dynamic/architecture/public/ArchiList';
-import ArchiForm from './pages/dynamic/architecture/private/ArchiForm/ArchiForm';
-import About from './pages/static/about/About';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/static/home/Home";
+import Auth from "./pages/auth/Auth";
+import Header from "./layout/header/Header";
+import NotFound from "./pages/static/notFound/NotFound";
+import ArchiList from "./pages/dynamic/architecture/public/ArchiList";
+import ArchiForm from "./pages/dynamic/architecture/private/ArchiForm/ArchiForm";
+import About from "./pages/static/about/About";
 import Events from "./pages/dynamic/events/Events";
 import Paintings from "./pages/dynamic/paintings/Paintings";
-import Footer from "./layout/footer/Footer"
-
-
+import Footer from "./layout/footer/Footer";
+import FormEvents from "./pages/dynamic/events/private/form-events/Form-events";
 
 function App() {
   return (
     <Router>
       <Header></Header>
       <Routes>
-      <Route path='' element={<Home />} ></Route>
-      <Route path='/login' element={<Auth />}></Route>
-      <Route path='/about' element={<About />}></Route>
-    <Route path='/architecture' element={<ArchiList />}></Route>
-    <Route path='/archiprivate' element={<ArchiForm />}></Route>
+        <Route path="" element={<Home />}></Route>
+        <Route path="/login" element={<Auth />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/architecture" element={<ArchiList />}></Route>
+        <Route path="/archiprivate" element={<ArchiForm />}></Route>
         <Route path="/events" element={<Events></Events>}></Route>
-         <Route path="/paintings" element={<Paintings></Paintings>}></Route>
-      <Route path='*' element={<NotFound />}></Route>
+        <Route path="/eventsform" element={<FormEvents></FormEvents>}></Route>
+        <Route path="/paintings" element={<Paintings></Paintings>}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
       <Footer></Footer>
     </Router>
--- galeria/src/pages/dynamic/events/private/list-events/List-events.js
+++ galeria/src/pages/dynamic/events/private/list-events/List-events.js
@@ -1,5 +1,7 @@
 import { Component } from "react";
 import events from "../../datagen/EventData";
+import { Link } from "react-router-dom";
+import "./List-events.css";
 
 export default function ListEvents() {
   return (
@@ -7,6 +9,13 @@ export default function ListEvents() {
       <header>
         <h1>Lista de eventos</h1>
       </header>
+      <div id="btn-create-events">
+        <button value="crear-evento">
+          <Link id="link" to="/eventsform">
+            CREAR EVENTO
+          </Link>
+        </button>
+      </div>
       <div id="list-events-container">
         {events.map((event) => (
           <article key={event.id}>
