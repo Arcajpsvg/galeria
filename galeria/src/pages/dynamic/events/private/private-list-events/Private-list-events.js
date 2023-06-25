33cd00acbb394c4d726090ae771d18bf15065446
cjamar <121177196+cjamar@users.noreply.github.com>
2023-06-25 18:57:21 +0200

    Build eventslist private table

--- galeria/src/pages/dynamic/events/private/private-list-events/Private-list-events.js
+++ galeria/src/pages/dynamic/events/private/private-list-events/Private-list-events.js
@@ -1,19 +1,43 @@
 import { Component } from "react";
+import events from "../../datagen/EventData";
 
 export default class PrivateListEvents extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      storageEvents: JSON.parse(localStorage.getItem("events"))
-        ? Array.from(JSON.parse(localStorage.getItem("events")))
-        : [],
+      storageEvents: localStorage.getItem("events")
+        ? events.concat(Array.from(JSON.parse(localStorage.getItem("events")))) 
+        : events.concat([]),
     };
   }
-  //   finalEventList = events.concat(storageEvents);
 
   render() {
     return (
-      <></>
+      <table id="private-archi-table">
+        <tbody>
+          <tr>
+            <th>Id</th>
+            <th>Título</th>
+            <th>Actions</th>
+          </tr>
+          {events.map((building) => (
+            <tr key={building.id}>
+              <td>{building.id}</td>
+              <td>{building.titulo}</td>
+              <td>
+                <div className="action-control">
+                  <button onClick={() => this.props.editBuilding(building.id)}>
+                    Edit
+                  </button>
+                  <button onClick={() => this.deleteBuilding(building.id)}>
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
