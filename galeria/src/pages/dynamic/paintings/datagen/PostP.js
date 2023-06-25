605b40e37346e2806e3e91d5cac3445b0c838715
Alejandro <66041157+Arcajpsvg@users.noreply.github.com>
2023-06-25 13:39:11 +0200

    Show paintings list

--- galeria/src/pages/dynamic/paintings/datagen/PostP.js
+++ galeria/src/pages/dynamic/paintings/datagen/PostP.js
@@ -5,12 +5,12 @@ let PostP = [1,2,3,4,5,6,7,8,9,10];
 PostP = PostP.map(post =>{
     return {
         id: post,
-        image: faker.image.urlLoremFlickr(),
-        title: faker.lorem.sentence(),
-        author: faker.person.firstName(),
-        style:faker.lorem.sentence(),
-        price:faker.commerce.price(),
-        year: faker.date.past({ years: 80 }).toString()
+        imagen: faker.image.urlLoremFlickr(),
+        titulo: faker.lorem.sentence(),
+        autor: faker.person.firstName(),
+        estilo:faker.lorem.sentence(),
+        precio:faker.commerce.price(),
+        anno: faker.date.past({ years: 80 }).toString()
     }
 });
 
